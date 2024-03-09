0f8c83ee7c67389fc584c3e9b8b36de4535246a7
“resinprotein2333” <resinprotein2333@gmail.com>
2024-03-09 19:18:28 +0800

    update BOT file

--- Start-Bot.py
+++ Start-Bot.py
@@ -18,13 +18,15 @@ def extract_arg(arg):
 ## '/help' Command
 @bot.message_handler(commands=['help'])
 def Send_help(message):
-    help_info ='''使用说明：
-    - CVE漏洞查询机器人，用于查询相关CVE漏洞信息和可利用的EXP
+    #程序使用
+    help_info ='''使用说明:
+    - CVE漏洞查询机器人,用于查询相关CVE漏洞信息和可利用的EXP
 
-    命令列表：
+    命令列表:
     /help               查看帮助
-    /search [CVE ID]    
-    /cvetoday                   '''
+    /search [CVE ID]    搜索漏洞的相关信息
+    '''
+    # 发送信息
     bot.send_message(message.chat.id, help_info)
 
 ##'/search' Command
@@ -34,13 +36,12 @@ def yourCommand(message):
     user_input = "".join(parameter)
     url = ("https://api.msrc.microsoft.com/sug/v2.0/zh-cn/vulnerability/" + user_input)
     data = requests.get(url).json()
-    ###
+    
     vlun_number = data["cveNumber"]
-
     vlun_info = BeautifulSoup(data["description"], "html.parser").get_text(strip=True)
-
     vlun_type = data["cveTitle"]
-    ### Massage to send to user
+    
+    # 对信息进行格式化
     CVE_info = f"""
     漏洞编号：{vlun_number}
 
@@ -48,11 +49,11 @@ def yourCommand(message):
 {vlun_type}
 
 漏洞简介：
-{vlun_info}"""
+{vlun_info}""" 
+    # 发送格式化后的信息
     bot.send_message(message.chat.id, CVE_info)
 
-
-
+# 程序主函数入口
 if __name__ == '__main__':
     print("[*] Bot is running now...")
     bot.polling()
